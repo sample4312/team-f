484af79566b1c62f59f0c94a321c167c0e709c05
sample4312 <tawatanabe@cy-com.co.jp>
2025-06-19 14:15:25 +0900

    3

--- src/test/java/com/example/music_management/MusicmanagementApplicationTests.java
+++ src/test/java/com/example/music_management/MusicmanagementApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.music_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.Trainingmanagement.TrainingManagementApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = TrainingManagementApplication.class)
 class MusicmanagementApplicationTests {
 
 	@Test
